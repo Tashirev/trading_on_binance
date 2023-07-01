0022bd76289619b309a533e347dea8d95785de9f
denis <tashirevd@mail.ru>
2023-07-01 09:00:26 +0300

    add try except in while True

--- production/feature_download/main_features.py
+++ production/feature_download/main_features.py
@@ -27,17 +27,19 @@ def main():
     ticker_count = last_ticker() + 1 # должно быть значение послекднего тикера из БД ticker столбец id_ticker +1
 
     while True:
-        # запрос данных из binance
-        response = binance_download(pairs_binance, pairs_name)
+        try:
+            # запрос данных из binance
+            response = binance_download(pairs_binance, pairs_name)
 
-        server_time = datetime.fromtimestamp(response[0] / 1000)
-        # запись данных в БД binance
-        sql_status = sql_write(ticker_count, server_time, response[1])
-        # увеличение счётчика записей на 1
-        ticker_count += 1
-        sleep(0.5)
-        loger_stdout.info(f'SQL status: {sql_status}, ticker_count: {ticker_count}, time: {server_time}')
-
+            server_time = datetime.fromtimestamp(response[0] / 1000)
+            # запись данных в БД binance
+            sql_status = sql_write(ticker_count, server_time, response[1])
+            # увеличение счётчика записей на 1
+            ticker_count += 1
+            sleep(0.5)
+            loger_stdout.info(f'SQL status: {sql_status}, ticker_count: {ticker_count}, time: {server_time}')
+        except Exception as exc:
+            loger_stdout.info(f'exc')
 
 if __name__ == '__main__':
     main()
